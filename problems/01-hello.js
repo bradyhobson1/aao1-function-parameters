5d36141e3808817cc98991d123e5ece138346188
myrandathompson <myrandathompson@icloud.com>
2024-06-08 13:52:18 -0700

    test

--- problems/01-hello.js
+++ problems/01-hello.js
@@ -3,8 +3,8 @@ Write a function hello that takes in a string parameter.
 It should print out "Hello, " followed by the value of the parameter.
 HINT: this function doesn't need to return anything, it should just print using
 console.log.
+// test
 */
-
 // Your code here 
 
 // Note: If you console.log the return value of these function calls; you will get undefined. Remember a function that does not return anything evaluates to undefined.
